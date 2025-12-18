cec3c5e738a2bd123fbf30826ef9da08b4868dbb
Vaidik Pandey <pandeyvaidik04@gmail.com>
2025-12-18 15:47:12 +0530

    [Vaidik Pandey].add Programming elements level1 problems on day2.

---
+++ core-java-practice/gcr-codebase/programming-elements/level1/AveragePercentMarks.java
@@ -0,0 +1,11 @@
+package level1;
+public class AveragePercentMarks {
+    public static void main(String[] args){
+        int Maths = 94;
+        int Physics = 95;
+        int Chemistry = 96;
+        int average = (Maths + Physics + Chemistry) / 3;
+        float AveragePercentMarks = (average / 100.0f) * 100;
+        System.out.println("Sam's Average Percent Marks in PCM is: " + AveragePercentMarks);
+    }
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/level1/CalculateProfitandLoss.java
@@ -0,0 +1,11 @@
+package level1;
+public class CalculateProfitandLoss {
+    public static void main(String[] args) {
+        int costPrice = 129;
+        int sellingPrice = 191;
+        int Profit = sellingPrice - costPrice;
+        float ProfitPercentage = (float) Profit / costPrice * 100;
+        System.out.println("Cost Price is INR " + costPrice+ ", Selling Price is INR " + sellingPrice);
+        System.out.println("the Profit is INR " + Profit + " and Profit Percentage is " + ProfitPercentage + "%");
+    }
+}
