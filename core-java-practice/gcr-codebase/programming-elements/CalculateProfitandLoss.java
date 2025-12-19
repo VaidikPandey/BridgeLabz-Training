e4ce363e2739d69e64da4fe90a6a1a6b4cdd0d39
Vaidik Pandey <pandeyvaidik04@gmail.com>
2025-12-19 10:41:19 +0530

    Revert "[Vaidik Pandey].add refactoring changes to Programming elements level2 problems on day3."
    This reverts commit 063c5c6f6e642679f41dedd8db014b3515e75b71.

--- core-java-practice/gcr-codebase/programming-elements/CalculateProfitandLoss.java
+++ core-java-practice/gcr-codebase/programming-elements/CalculateProfitandLoss.java
@@ -2,9 +2,9 @@ public class CalculateProfitandLoss {
     public static void main(String[] args) {
         int costPrice = 129;
         int sellingPrice = 191;
-        int profit = sellingPrice - costPrice;
-        float profitPercentage = (float) profit / costPrice * 100;
+        int Profit = sellingPrice - costPrice;
+        float ProfitPercentage = (float) Profit / costPrice * 100;
         System.out.println("Cost Price is INR " + costPrice+ ", Selling Price is INR " + sellingPrice);
-        System.out.println("the Profit is INR " + profit + " and Profit Percentage is " + profitPercentage + "%");
+        System.out.println("the Profit is INR " + Profit + " and Profit Percentage is " + ProfitPercentage + "%");
     }
 }
